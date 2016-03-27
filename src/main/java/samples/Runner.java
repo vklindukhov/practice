fa35f28a5f188a9a10b977f6c345c73e1096be13
Herta2006 <v.klinduhov@gmail.com>
2016-03-27 16:26:37 +0300

    test commit

--- src/main/java/samples/Runner.java
+++ src/main/java/samples/Runner.java
@@ -1,13 +1,8 @@
 package samples;
 
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import java.util.function.Supplier;
-
 public class Runner {
     public static void main(String[] args) {
-
+        System.out.println("Test commit");
     }
 }
 
